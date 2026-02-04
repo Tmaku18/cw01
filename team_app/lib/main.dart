cca382efdcb88ebee7d6b4895919de0682a3e96b
Bruno Rico <ricobruno58@gmail.com>
2026-02-04 14:56:45 -0500

    added tabs skeleton, unusable

--- team_app/lib/main.dart
+++ team_app/lib/main.dart
@@ -7,6 +7,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,6 +24,15 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
